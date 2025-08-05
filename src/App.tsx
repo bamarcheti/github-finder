2b99c7c0dc1b1c298035ed91448fff06d4924795
bamarcheti <bmarchetifiorin@gmail.com>
2025-08-05 13:07:53 -0400

    fix: Ajustando estilização do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="mx-auto max-w-[500px]">
+    <div className="mx-auto max-w-[500px] pt-12 md:pt-0">
       <h1 className="mb-4 text-center text-[2rem] font-bold">Github Finder</h1>
       <Outlet />
     </div>
